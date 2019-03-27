bb451fec6a4ee96ba6ef0c07801847da1466a100
leibo <252517584@qq.com>
2019-03-27 15:39:51 +0800

    vux-ui 配置

---
+++ plugins/vux-components.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import { Group,Cell,Divider,Swiper } from 'vux'
+
+Vue.component('group',Group)
+Vue.component('cell',Cell)
+Vue.component('divider',Divider)
+Vue.component('swiper',Swiper)
+
